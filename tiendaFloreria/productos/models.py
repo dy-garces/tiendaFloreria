f64a378b09dc180fd41f0d99e3fb015358be4d6c
Dyllan Garces <dy.garces@duocuc.cl>
2022-06-12 00:04:58 -0400

    agregado de slug unico

--- tiendaFloreria/productos/models.py
+++ tiendaFloreria/productos/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.utils.text import slugify
+from django.db.models.signals import pre_save
+import uuid
 
 # Create your models here.
 
@@ -17,9 +19,16 @@ class Producto(models.Model):
     creacion = models.DateTimeField(auto_now_add=True)
     categoria =  models.ForeignKey(Categoria, on_delete=models.PROTECT)
     
-    def save(self, *args, **kwargs ):
-        self.slug = slugify(self.nombre)
-        super(Producto, self).save(*args, **kwargs)
-    
     def __str__(self):
         return self.nombre
+    
+def set_slug(sender, instance, *args, **kwargs):
+    if instance.nombre and not instance.slug:
+        slug = slugify(instance.nombre) 
+        while Producto.objects.filter(slug=slug).exists():
+            slug = slugify(
+                '{}-{}'.format(instance.nombre, str(uuid.uuid4())[:8] )
+            )
+        instance.slug = slug
+        
+pre_save.connect(set_slug,sender=Producto)
