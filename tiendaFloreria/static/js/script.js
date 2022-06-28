f6ff0e98ef4b4653228ed5b2b339dba34e6c3c82
Roberto Fuentealba <ro.fuentealba@duocuc.cl>
2022-06-27 23:52:33 -0400

    Cache + Api Del Mapa

--- tiendaFloreria/static/js/script.js
+++ tiendaFloreria/static/js/script.js
@@ -1,7 +1,7 @@
 function iniciarMap() {
-    var coord = { lat: -34.5956145, lng: -58.4431949 };
+    var coord = { lat: -36.7909433, lng: -73.0564579 };
     var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 10,
+        zoom: 17,
         center: coord
     });
     var marker = new google.maps.Marker({
